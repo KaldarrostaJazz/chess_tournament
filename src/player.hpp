3e1dd435b0b54c526501506f2712eee0dd9b6347
Giuseppe Sguera <giuseppe.sguera2@studio.unibo.it>
2021-06-15 15:52:36 +0200

    player.hpp

--- src/player.hpp
+++ src/player.hpp
@@ -7,9 +7,9 @@ class Player {
   int Elo;
 
  public:
-  Player(std::string n = "Default", int e = 1500) : Name{n}, Elo{e} {} 
+  Player(std::string& n = "Default", int e = 1500) : Name{n}, Elo{e} {} 
     std::string name() { return Name; }
-    void name(std::string s) { Name = s; }
+    void name(std::string& s) { Name = s; }
     int elo() { return Elo; }
     void elo(int n) {Elo = n; }
 };
