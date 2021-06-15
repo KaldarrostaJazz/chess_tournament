be36c3ce344a2f682ae00cb0743978614e5a48b1
Giuseppe Sguera <giuseppe.sguera2@studio.unibo.it>
2021-06-15 15:52:50 +0200

    tournament.hpp

--- src/tourament.hpp
+++ src/tourament.hpp
@@ -1,4 +1,14 @@
 #ifndef TOURNAMENT_HPP
 #define TOURNAMENT_HPP
+#include "player.hpp"
+#include <vector>
 class Tournament () {
-
+	private:
+		std::vector<Player> entrants;
+	public:
+		Tournament() = default;
+		void add_entrant(Player const& player) {
+			entrants.push_back(player);
+		}
+};
+#endif
